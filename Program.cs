6c60a4b09f61e1a25bc8b66ba6b055abeef2c483
João Paulo <joaopdejesus79@gmail.com>
2021-08-27 20:23:56 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparar...");
+            Console.ReadLine();
+            Console.WriteLine("Apontar...");
+            Console.ReadLine();
+            Console.WriteLine("FOGO!");
+        }
+    }
+}
